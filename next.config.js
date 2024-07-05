286fd6b73d539b3db380d1218a05922842b2423a
evad-e <cd.empinado@gmail.com>
2024-07-05 20:13:33 +0800

    feat: Add flask api

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5328/api/:path*'
+            : '/api/',
+      },
+    ]
+  },
+};
 
 export default config;
