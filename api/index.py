6465c044a852d3216fc2444e28492925a0106afa
evad-e <cd.empinado@gmail.com>
2024-07-27 20:48:58 +0800

    chore: Update package.json for dev env

--- api/index.py
+++ api/index.py
@@ -2,5 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World Test!</p>"
+def hello():
+    return {
+        "message": "Hello, World!",
+        "test": "hello"
+    }
+    
+@app.route("/api/python/<name>")
+def test_name(name):
+  return f"Hello {name}"
