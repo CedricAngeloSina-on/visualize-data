e3eec218e99e691bf2ed0579fdeb3f9022a2ce13
CedricAngeloSina-on <cao.sinaon@gmail.com>
2024-08-07 23:05:46 +0800

    feat: multi select button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,67 @@
 "use client";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
+import { ThemeToggle } from "~/components/ui/theme-toggle";
+import { MultiSelect } from "~/components/ui/multi-select";
 import { Button } from "~/components/ui/button";
 import {
     Form,
     FormControl,
+    FormDescription,
     FormField,
     FormItem,
     FormLabel,
     FormMessage,
 } from "~/components/ui/form";
-import { Input } from "~/components/ui/input";
-import { ThemeToggle } from "~/components/ui/theme-toggle";
 
-// const MAX_UPLOAD_SIZE = 1024 * 1024 * 3; // 3MB
-// const ACCEPTED_FILE_TYPES = ["csv"];
+const frameworksList = [
+    {
+        value: "next.js",
+        label: "Next.js",
+    },
+    {
+        value: "sveltekit",
+        label: "SvelteKit",
+    },
+    {
+        value: "nuxt.js",
+        label: "Nuxt.js",
+    },
+    {
+        value: "remix",
+        label: "Remix",
+    },
+    {
+        value: "astro",
+        label: "Astro",
+    },
+];
 
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
+const FormSchema = z.object({
+    frameworks: z
+        .array(z.string().min(1))
+        .min(1)
+        .nonempty("Please select at least one framework."),
 });
 
 export default function HomePage() {
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
         defaultValues: {
-            username: "",
+            frameworks: ["next.js", "nuxt.js"],
         },
     });
 
-    // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values);
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+        console.log(data);
     }
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-center">
             <div className="m-12 items-center text-5xl">
                 <> Upload, Configure, Visualize.</> <ThemeToggle />
-            </div>
-            <div className="grid w-full max-w-sm items-center gap-1.5">
                 <Form {...form}>
                     <form
                         onSubmit={form.handleSubmit(onSubmit)}
@@ -51,25 +69,40 @@ export default function HomePage() {
                     >
                         <FormField
                             control={form.control}
-                            name="username"
+                            name="frameworks"
                             render={({ field }) => (
                                 <FormItem>
-                                    <FormLabel>CSV</FormLabel>
+                                    <FormLabel>Frameworks</FormLabel>
                                     <FormControl>
-                                        <Input
-                                            id="csv"
-                                            placeholder="shadcn"
-                                            {...field}
+                                        <MultiSelect
+                                            options={frameworksList}
+                                            onValueChange={field.onChange}
+                                            defaultValue={field.value}
+                                            placeholder="Select options"
+                                            variant="inverted"
+                                            animation={2}
+                                            maxCount={3}
                                         />
                                     </FormControl>
+                                    <FormDescription>
+                                        Choose the frameworks you are interested
+                                        in.
+                                    </FormDescription>
                                     <FormMessage />
                                 </FormItem>
                             )}
                         />
-                        <Button type="submit">Submit</Button>
+                        <Button
+                            variant="default"
+                            type="submit"
+                            className="w-full"
+                        >
+                            Submit
+                        </Button>
                     </form>
                 </Form>
             </div>
+            <div className="grid w-full max-w-sm items-center gap-1.5"></div>
         </main>
     );
 }
